720104f96e976210d1e5e657a9eac6ee6ae0fdeb
seba <villeaho@tutanota.com>
2019-08-23 16:15:26 +0300

    Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: "warm-river-rails-final.herokuapp.com"}
+  config.action_mailer.perform_deliveries = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
